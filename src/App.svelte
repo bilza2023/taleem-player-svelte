1f69e0b36c5a6bd84465bdd8cac5fa97c6364d62
bilza2023 <bilza2023@gmail.com>
2026-04-03 12:05:26 +0500

    working

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,32 @@
 <script>
+  import { onMount } from "svelte";
+  import Player from "./player/Player.svelte";
 
+  import { renderLoop, Timer } from "taleem-pam";
+  import { resolveAssetPaths } from "./lib/utils/index.js";
 
-import Player from './player/Player.svelte';
+  let deck = null;
+  let currentTime = 0;
 
+  const timer = new Timer();
 
+  async function loadDeck() {
+    const res = await fetch("/public/specs/goldenDeckV2.json");
+    const json = await res.json();
+    deck = resolveAssetPaths(json);
+  }
+
+  onMount(async () => {
+    await loadDeck();
+
+    renderLoop.start(() => {
+      currentTime = timer.now();
+    });
+
+    timer.play();
+  });
 </script>
 
-<div class="app">
-
-  <!-- <Editor bind:deck={deck}/> -->
-  <Player/>
-</div>
-
-<style>
-  
-
-</style>
+{#if deck}
+  <Player {deck} {currentTime} />
+{/if}
